84124ac0c2dec494f2f31941090c8f755bf33b7a
zwq00000 <zwq00000@hotmail.com>
2021-01-14 18:02:58 +0800

    fix ToDuration support week and not support ns

--- test/InfluxDb.Extensions.Tests/InfluxDbQueryExtensionsTests.cs
+++ test/InfluxDb.Extensions.Tests/InfluxDbQueryExtensionsTests.cs
@@ -2,8 +2,7 @@ using System;
 using Xunit;
 using Xunit.Abstractions;
 
-namespace InfluxDb.Extensions.Tests
-{
+namespace InfluxDb.Extensions.Tests {
     public class InfluxDbQueryExtensionsTests {
         private readonly ITestOutputHelper output;
 
@@ -26,13 +25,32 @@ namespace InfluxDb.Extensions.Tests
         }
 
         [Fact]
-        public void TestToDuration()
-        {
-            var dur = TimeSpan.FromMinutes(3);
-            Assert.Equal("3m", dur.ToDuration());
+        public void TestToDuration () {
+
+            var dur = TimeSpan.FromMilliseconds (100);
+            Assert.Equal ("100ms", dur.ToDuration ());
+
+            dur = TimeSpan.FromSeconds (1);
+            Assert.Equal ("1s", dur.ToDuration ());
+
+            dur = TimeSpan.FromMinutes (3);
+            Assert.Equal ("3m", dur.ToDuration ());
+
+            dur = TimeSpan.FromHours (1);
+            Assert.Equal ("1h", dur.ToDuration ());
+
+            dur = TimeSpan.FromDays (1);
+            Assert.Equal ("1d", dur.ToDuration ());
+
+            dur = TimeSpan.FromDays (7);
+            Assert.Equal ("1w", dur.ToDuration ());
+
+            dur = TimeSpan.FromDays (14);
+            Assert.Equal ("2w", dur.ToDuration ());
+
+            dur = TimeSpan.FromDays (13);
+            Assert.Equal ("13d", dur.ToDuration ());
 
         }
     }
-
-    
 }
